995bb58bffedd21f0ff3a81885018297f13aa0df
Ramsay Lanier <ramsay@nclud.com>
2016-07-04 10:44:36 -0400

    init

---
+++ src/components/WordExpressPage.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { connect } from 'react-apollo';
+import gql from 'graphql-tag';
+
+class WordExpressPage extends React.Component{
+	render(){
+    const { getPage } = this.props;
+    const { Layouts } = this.props.routes[0];
+    let Layout;
+
+    if (getPage.loading){
+      return <div></div>
+    } else{
+      const { page } = getPage;
+
+      if (!page){
+        Layout = Layouts['NotFound'];
+      } else if (page.layout){
+        Layout = Layouts[page.layout.meta_value] || Layouts['Default'];
+      } else {
+        Layout = Layouts['Default'];
+      }
+
+      return <Layout.Component page={page} layout={Layout}/>
+    }
+	}
+}
+
+const WordExpressPageWithData = connect({
+  mapQueriesToProps({ ownProps, state}){
+    return {
+      getPage: {
+        query:gql`
+          query getPage($page: String){
+            page(name: $page){
+              id,
+    					post_title
+    					post_content
+    					thumbnail,
+              layout{
+                meta_value
+              }
+            }
+          }
+        `,
+        variables: {
+          page: ownProps.params.page || 'Homepage'
+        }
+      }
+    }
+  }
+})(WordExpressPage);
+
+export default WordExpressPageWithData;
